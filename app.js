ede3bba17143f38d36d1a741f3d3081f4a53e16f
Jino1211 <77382695+Jino1211@users.noreply.github.com>
2021-03-23 14:58:36 +0200

    backend 3 test passed
    added error handling for id that not find,
    and for database mongoose error.

--- app.js
+++ app.js
@@ -18,35 +18,40 @@ app.get("/api/tickets", async (req, res) => {
     res.status(200).send(data);
   } catch (e) {
     console.log(e);
-    res.status(400).send("Server Problem");
+    res.status(500).send("Server Problem");
   }
 });
 
 app.patch("/api/tickets/:ticketsId/done", (req, res) => {
   const { ticketsId } = req.params;
-  const updateTicket = {
-    done: true,
-  };
-  Tickets.updateOne({ ticketsId }, updateTicket, { new: true })
-    .then(() => {
-      res.status(201).send({ updated: true });
+
+  Tickets.updateOne({ _id: ticketsId }, { done: true }, { new: true })
+    .then((data) => {
+      console.log(data);
+      res.status(200).send({ updated: true });
     })
-    .cath((e) => {
-      res.status(404).send({ updated: true });
+    .catch((e) => {
+      if (e.name === "CastError") {
+        res.status(404).send({ message: "ID not found", updated: false });
+      } else {
+        res.status(500).send({ Error: "Mongoose error", updated: false });
+      }
     });
 });
 
 app.patch("/api/tickets/:ticketsId/undone", (req, res) => {
   const { ticketsId } = req.params;
-  const updateTicket = {
-    done: false,
-  };
-  Tickets.updateOne({ ticketsId }, updateTicket, { new: true })
+
+  Tickets.updateOne({ _id: ticketsId }, { done: false }, { new: true })
     .then(() => {
-      res.status(201).send({ updated: true });
+      res.status(200).send({ updated: true });
     })
-    .cath((e) => {
-      res.status(404).send({ updated: true });
+    .catch((e) => {
+      if (e.name === "CastError") {
+        res.status(404).send({ message: "ID not found", updated: false });
+      } else {
+        res.status(500).send({ Error: "Mongoose error", updated: false });
+      }
     });
 });
 
